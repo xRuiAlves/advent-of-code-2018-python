63250cb3e925627d23e3146e828815545afe5e3f
xRuiAlves <up201606746@fe.up.pt>
2018-12-09 08:01:26 +0000

    Added Day 9 Part 1 Solution

---
+++ Day9/Day9_Part1.py
@@ -0,0 +1,42 @@
+import re
+
+class Node: 
+    def __init__(self, data): 
+        self.data = data
+        self.next = None
+        self.last = None
+
+try:
+    file = open("Input_Day9" , "r")
+except IOError:
+    print("*** ERROR: Could not open file for reading input ***")
+    raise SystemExit
+
+input = file.readline()
+[num_players, num_marbles] = map(int, re.findall('\\d+', input))
+
+players = [0] * num_players
+n0 = Node(0)
+n1 = Node(1)
+n0.next = n1
+n0.last = n1
+n1.next = n0
+n1.last = n0
+current_node = n1
+
+for marble in range(2, num_marbles + 1):
+    if marble % 23 == 0:
+        for i in range(7):
+            current_node = current_node.last
+        players[marble % num_players] += marble + current_node.data
+        current_node.last.next = current_node.next
+        current_node = current_node.next
+    else:
+        newNode = Node(marble)
+        newNode.last = current_node.next
+        newNode.next = current_node.next.next
+        current_node.next.next.last = newNode
+        current_node.next.next = newNode
+        current_node = newNode
+
+print(max(players))
